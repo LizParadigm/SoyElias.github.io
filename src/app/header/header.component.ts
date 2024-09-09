97374e8f8ed8c19f4dcf83b51d72ee0d45823717
LizParadigm <jesss635000@gmail.com>
2024-09-09 09:46:33 -0600

    update header for my

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  img_usuario!: string;
+
+
+  ngOnInit(): void {
+    this.jojoRandom();
+  }
+
+  jojoRandom(): void {
+    let number = Math.floor(Math.random() * 2) + 1;
+    this.img_usuario = "assets/jojo/" + number + ".jpg"
+    console.log(this.img_usuario)
+  }
 
 }
